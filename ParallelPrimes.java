15f90be89806f52d95a23940678c41900a6c393c
Jarod Davies <balancejackfireblade@gmail.com>
2024-01-26 20:19:57 -0500

    Print now happens once at end
    I couldn't really get join() to work how I wanted, but this Java utility ended up being exactly what I was looking for.

--- ParallelPrimes.java
+++ ParallelPrimes.java
@@ -1,29 +1,36 @@
 import java.lang.Thread;
+import java.util.concurrent.CountDownLatch;
 
 public class ParallelPrimes extends Thread {
     private static final long maxNum = 100000000;
     private static final int numThreads = 8;
     private Counter counter;
+    private CountDownLatch latch;
 
-    public ParallelPrimes(Counter counter) {
+    public ParallelPrimes(Counter counter, CountDownLatch latch) {
         this.counter = counter;
+        this.latch = latch;
     }
 
     public void run() {
         primePrint(this.counter);
+        this.latch.countDown();
     }
 
-    public static void main(String [] args) {
+    public static void main(String [] args) throws InterruptedException {
         Counter counter = new Counter();
         ParallelPrimes [] primeCheckers = new ParallelPrimes[numThreads];
+        CountDownLatch latch = new CountDownLatch(numThreads);
         for (int i = 0; i < numThreads; i++) {
-            primeCheckers[i] = new ParallelPrimes(counter);
+            primeCheckers[i] = new ParallelPrimes(counter, latch);
         }
         for (int i = 0; i < numThreads; i++) {
             primeCheckers[i].start();
         }
 
+        latch.await();
         System.out.println(counter.getSum());
+        System.out.println(counter.getCount());
     }
 
     private static void primePrint(Counter counter) {
@@ -34,8 +41,6 @@ public class ParallelPrimes extends Thread {
             }
             i = counter.getAndIncrement();
         }
-        System.out.println(counter.getSum());
-        System.out.println(counter.getCount());
     }
 
     // Checks all odd divisors up to the square root of the number (odd because we only calculate on odd numbers)
