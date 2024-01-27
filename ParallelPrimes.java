7b8029695e7896d6b06cefffcfcaf3aafc25b9a0
Jarod Davies <balancejackfireblade@gmail.com>
2024-01-26 20:48:05 -0500

    More formatting fixes
    Apparently the top primes thing still isn't working but it'll be done soon

--- ParallelPrimes.java
+++ ParallelPrimes.java
@@ -1,13 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.lang.Thread;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 
 public class ParallelPrimes extends Thread {
     private static final long maxNum = 100000000;
     private static final int numThreads = 8;
-    private Counter counter;
+    private PrimeCounter counter;
     private CountDownLatch latch;
 
-    public ParallelPrimes(Counter counter, CountDownLatch latch) {
+    public ParallelPrimes(PrimeCounter counter, CountDownLatch latch) {
         this.counter = counter;
         this.latch = latch;
     }
@@ -17,8 +23,13 @@ public class ParallelPrimes extends Thread {
         this.latch.countDown();
     }
 
-    public static void main(String [] args) throws InterruptedException {
-        Counter counter = new Counter();
+    public static void main(String [] args) throws InterruptedException, FileNotFoundException {
+        FileOutputStream out = new FileOutputStream("primes.txt");
+        PrintStream outPrint = new PrintStream(out);
+        PrimeCounter counter = new PrimeCounter();
+
+        Instant startTime = Instant.now();
+
         ParallelPrimes [] primeCheckers = new ParallelPrimes[numThreads];
         CountDownLatch latch = new CountDownLatch(numThreads);
         for (int i = 0; i < numThreads; i++) {
@@ -29,11 +40,17 @@ public class ParallelPrimes extends Thread {
         }
 
         latch.await();
-        System.out.println(counter.getSum());
-        System.out.println(counter.getCount());
+
+        Instant endTime = Instant.now();
+        double totalTime = Duration.between(startTime, endTime).toMillis() / 1000.0;
+
+        outPrint.println(totalTime + "s " + counter.getPrimesFound() + " " + counter.getSum());
+        int [] topPrimes = counter.getTopPrimes();
+        Arrays.sort(topPrimes);
+        outPrint.println(Arrays.toString(topPrimes));
     }
 
-    private static void primePrint(Counter counter) {
+    private static void primePrint(PrimeCounter counter) {
         long i = counter.getAndIncrement();
         while (i < maxNum) {
             if (isPrime(i)) {
---
+++ PrimeCounter.java
@@ -0,0 +1,34 @@
+public class PrimeCounter {
+    // We want our primes to count on odd numbers, and just already include 2 in the calculation.
+    // This ensures no need to check even numbers at any point.
+    private long currentNum = 3;
+    private long sum = 2;
+    private long primesFound = 1;
+
+    // Store the top 10 primes in a list, sorting them later to make my life easier
+    private long[] topPrimes = new long[10];
+
+    public synchronized long getAndIncrement() {
+        long temp = currentNum;
+        currentNum = temp + 2;
+        return temp;
+    }
+
+    public synchronized void addPrime(long num) {
+        primesFound += 1;
+        sum += num;
+        topPrimes[(int)(primesFound % 10)] = num;
+    }
+
+    public long getSum() {
+        return sum;
+    }
+
+    public long getPrimesFound() {
+        return primesFound;
+    }
+
+    public int[] getTopPrimes() {
+        return topPrimes;
+    }
+}
